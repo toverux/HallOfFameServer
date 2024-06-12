07475699a8353dc0090beac2d318056bcd43dffb
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2024-06-13 01:40:06 +0200

    feat(server): improve error handling in the case Angular SSR engine fails

--- projects/server/main.ts
+++ projects/server/main.ts
@@ -6,11 +6,10 @@ import { CommonEngine } from '@angular/ssr';
 import {
     type ArgumentsHost,
     Catch,
-    type ExceptionFilter,
-    type HttpException,
+    type HttpServer,
     NotFoundException
 } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import {
     FastifyAdapter,
     type NestFastifyApplication
@@ -36,37 +35,64 @@ async function bootstrap() {
     });
 
     app.useGlobalFilters(
-        new NotFoundExceptionFilter(browserDistFolder, indexHtml)
+        new NotFoundExceptionFilter(
+            app.getHttpAdapter(),
+            browserDistFolder,
+            indexHtml
+        )
     );
 
     await app.listen(4000);
 }
 
+/**
+ * Error filter that's meant to catch 404 errors from the static file router,
+ * and render the Angular application instead, either SSG or SSR.
+ * This is the most robust way I've found for now to handle static files + SPA
+ * routing with the same base URL in Nest, a previous middleware attempt did not
+ * succeed.
+ */
 @Catch(NotFoundException)
-class NotFoundExceptionFilter implements ExceptionFilter {
+class NotFoundExceptionFilter extends BaseExceptionFilter {
     private readonly commonEngine = new CommonEngine();
 
     public constructor(
+        applicationRef: HttpServer,
         private readonly browserDistFolder: string,
         private readonly indexHtml: string
-    ) {}
+    ) {
+        super(applicationRef);
+    }
 
-    public async catch(_: HttpException, host: ArgumentsHost) {
+    /**
+     * ###### Implementation Notes
+     * A synchronous error filter can rethrow errors, we can't as Angular SSR
+     * engine is asynchronous, therefore we need to catch the error and let the
+     * default Nest.js error handler, that we inherit from, handle it.
+     * Well, even then it's just the default implementation, not what might be
+     * configured as the default error handler elsewhere, but it's the best I've
+     * found so far.
+     */
+    public override async catch(_: NotFoundException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const req = ctx.getResponse<FastifyRequest>();
         const res = ctx.getResponse<FastifyReply>();
 
         const { protocol, originalUrl, headers } = req;
 
-        const result = await this.commonEngine.render({
-            bootstrap: bootstrapClient,
-            documentFilePath: this.indexHtml,
-            url: `${protocol}://${headers.host}${originalUrl}`,
-            publicPath: this.browserDistFolder,
-            providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
-        });
+        try {
+            const result = await this.commonEngine.render({
+                bootstrap: bootstrapClient,
+                documentFilePath: this.indexHtml,
+                url: `${protocol}://${headers.host}${originalUrl}`,
+                publicPath: this.browserDistFolder,
+                providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
+            });
 
-        res.header('Content-Type', 'text/html');
-        res.send(result);
+            res.header('Content-Type', 'text/html');
+            res.send(result);
+        } catch (error) {
+            super.catch(error, host);
+        }
     }
 }

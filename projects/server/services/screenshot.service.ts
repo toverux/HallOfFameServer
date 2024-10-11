toverux/HallOfFameServer

commit e70d2d308e79cb34e26b4c24ad7b176390300745

fix(api): increase max delay for saving a screenshot to 10s

projects/server/services/screenshot.service.ts
@@ -103,7 +103,17 @@ export class ScreenshotService {
 
         // Create the screenshot in the database and upload the screenshots,
         // in a transaction so if the upload fails, the database is not updated.
-        return this.prisma.$transaction(async prisma => {
+        return this.prisma.$transaction(saveScreenshot, {
+            // This is the timeout after which the whole transaction is
+            // cancelled; default is 5s, wait a little longer because images
+            // can take some time to upload when the network is slow.
+            timeout: 10_000
+        });
+
+        async function saveScreenshot(
+            this: ScreenshotService,
+            prisma: Prisma.TransactionClient
+        ): Promise<Screenshot> {
             // Create the screenshot in the database.
             const screenshotWithoutBlobs = await prisma.screenshot.create({
                 select: { id: true, cityName: true },
@@ -145,7 +155,7 @@ export class ScreenshotService {
             );
 
             return screenshot;
-        });
+        }
     }
 
     public async deleteScreenshot(

commit f5a904034c0ab0f7aa05b9cad3c6c4a1a83dfc25

fix(api): regression (crash) when uploading images to Azure
Regression from e70d2d308e79cb34e26b4c24ad7b176390300745

projects/server/services/screenshot.service.ts
@@ -107,14 +107,14 @@ export class ScreenshotService {
 
         // Create the screenshot in the database and upload the screenshots,
         // in a transaction so if the upload fails, the database is not updated.
-        return this.prisma.$transaction(saveScreenshot, {
+        return this.prisma.$transaction(saveScreenshotTransaction.bind(this), {
             // This is the timeout after which the whole transaction is
             // cancelled; default is 5s, wait a little longer because images
             // can take some time to upload when the network is slow.
             timeout: 10_000
         });
 
-        async function saveScreenshot(
+        async function saveScreenshotTransaction(
             this: ScreenshotService,
             prisma: Prisma.TransactionClient
         ): Promise<Screenshot> {
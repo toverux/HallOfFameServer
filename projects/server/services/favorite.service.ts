0c44d966c8a90600dace567b2e57cf4a29f3e680
Morgan Touverey Quilling <morgan@mtq.io>
2024-10-13 01:05:06 +0200

    fix(api): also ignore 172.* IPs from favorite status retrieval

--- projects/server/services/favorite.service.ts
+++ projects/server/services/favorite.service.ts
@@ -26,8 +26,8 @@ export class FavoriteService {
                 // biome-ignore lint/style/useNamingConvention: prisma
                 OR: [
                     { screenshotId, creatorId },
-                    { screenshotId, ip },
-                    { screenshotId, hwid }
+                    { screenshotId, hwid },
+                    ...(ip.startsWith('172') ? [] : [{ screenshotId, ip }])
                 ]
             }
         });

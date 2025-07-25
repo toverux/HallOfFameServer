aed3c7e32aa85ffbc9c729afb2e890cfcdd8c4e1
Morgan Touverey Quilling <morgan@mtq.io>
2025-07-25 14:21:51 +0200

    feat: don't send debug/verbose logs to sentry unless they have a [sentry] tag in the messsage

--- projects/server/logger.ts
+++ projects/server/logger.ts
@@ -7,17 +7,6 @@ interface AppLoggerOptions extends ConsoleLoggerOptions {
 }
 
 export class SentryConsoleLogger extends ConsoleLogger {
-  private static readonly logLevelToSentryLogFn: {
-    [K in LogLevel]: (message: string, attributes: Record<string, unknown>) => void;
-  } = {
-    debug: sentry.logger.debug,
-    verbose: sentry.logger.debug,
-    log: sentry.logger.info,
-    warn: sentry.logger.warn,
-    error: sentry.logger.error,
-    fatal: sentry.logger.fatal
-  };
-
   private readonly sentryFilterContexts: Set<string>;
 
   public constructor(options: AppLoggerOptions) {
@@ -27,45 +16,54 @@ export class SentryConsoleLogger extends ConsoleLogger {
   }
 
   public override debug(message: unknown, ...rest: unknown[]): void {
-    this.handleLog('debug', message, rest);
+    this.handleLog('debug', sentry.logger.debug, message, rest);
   }
 
   public override verbose(message: unknown, ...rest: unknown[]): void {
-    this.handleLog('verbose', message, rest);
+    this.handleLog('verbose', sentry.logger.debug, message, rest);
   }
 
   public override log(message: unknown, ...rest: unknown[]): void {
-    this.handleLog('log', message, rest);
+    this.handleLog('log', sentry.logger.info, message, rest);
   }
 
   public override warn(message: unknown, ...rest: unknown[]): void {
-    this.handleLog('warn', message, rest);
+    this.handleLog('warn', sentry.logger.warn, message, rest);
   }
 
   public override error(message: unknown, ...rest: unknown[]): void {
-    this.handleLog('error', message, rest);
+    this.handleLog('error', sentry.logger.error, message, rest);
   }
 
   public override fatal(message: unknown, ...rest: unknown[]): void {
-    this.handleLog('fatal', message, rest);
+    this.handleLog('fatal', sentry.logger.fatal, message, rest);
   }
 
-  private handleLog(level: LogLevel, message: unknown, rest: unknown[]): void {
+  private handleLog(
+    level: LogLevel,
+    sentryLogFn: (msg: string, attributes: Record<string, unknown>) => void,
+    message: unknown,
+    rest: unknown[]
+  ): void {
     super[level].call(this, message, ...rest);
+
+    const messageStr = typeof message == 'string' ? message : util.inspect(message);
+
+    if ((level == 'debug' || level == 'verbose') && !messageStr.includes('[sentry]')) {
+      return;
+    }
 
     const contextCandidate = rest.length > 0 ? rest.at(-1) : undefined;
     const context = typeof contextCandidate == 'string' ? contextCandidate : undefined;
 
     if (context && !this.sentryFilterContexts.has(context)) {
-      const messageStr = typeof message == 'string' ? message : util.inspect(message);
-
       const attributes: Record<string, unknown> = {};
 
       for (let i = 0; i < rest.length - 1; i++) {
         attributes[`param${i}`] = rest[i];
       }
 
-      SentryConsoleLogger.logLevelToSentryLogFn[level](`[${context}] ${messageStr}`, attributes);
+      sentryLogFn(messageStr, attributes);
     }
   }
 }

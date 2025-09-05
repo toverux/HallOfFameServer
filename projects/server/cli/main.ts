51afb84a942eb913a9c5f77c8ece5295f35bcb3f
Morgan Touverey Quilling <morgan@mtq.io>
2025-09-05 20:31:26 +0200

    fix(cli): correctly infer scope in logger and print other arguments than just the first one

--- projects/server/cli/main.ts
+++ projects/server/cli/main.ts
@@ -1,5 +1,6 @@
 import process from 'node:process';
-import chalk from 'chalk';
+import * as util from 'node:util';
+import chalk, { type ChalkInstance } from 'chalk';
 import { CommandFactory } from 'nest-commander';
 import { iconsole } from '../../shared/iconsole';
 import { StandardError } from '../common';
@@ -12,18 +13,31 @@ void CommandFactory.run(CliModule, {
   logger: {
     ...(config.verbose
       ? {
-          debug: (message, scope) => iconsole.debug(chalk.dim(message), chalk.dim(`[${scope}]`)),
-          verbose: (message, scope) => iconsole.debug(chalk.dim(message), chalk.dim(`[${scope}]`))
+          debug: log.bind(null, chalk.dim),
+          verbose: log.bind(null, chalk.dim)
         }
       : {}),
-    log: (message, scope) => iconsole.log(message, chalk.dim(`[${scope}]`)),
-    warn: (message, scope) => iconsole.warn(chalk.yellowBright(message), chalk.dim(`[${scope}]`)),
-    error: (message, scope) => iconsole.error(chalk.redBright(message), chalk.dim(`[${scope}]`)),
-    fatal: (message, scope) => iconsole.error(chalk.bgRedBright(message), chalk.dim(`[${scope}]`))
+    log: log.bind(null, chalk),
+    warn: log.bind(null, chalk.yellowBright),
+    error: log.bind(null, chalk.redBright),
+    fatal: log.bind(null, chalk.bgRedBright)
   },
   errorHandler: handleError,
   serviceErrorHandler: handleError
 });
+
+function log(formatter: ChalkInstance, ...args: unknown[]): void {
+  const context = args.pop();
+
+  for (const arg of args) {
+    const argStr =
+      typeof arg == 'string'
+        ? arg
+        : util.inspect(arg, { colors: true, depth: Number.POSITIVE_INFINITY });
+
+    iconsole.log(formatter(argStr), chalk.dim(`[${context}]`));
+  }
+}
 
 function handleError(error: Error): void {
   // If it's a command not found, we just have to return, printing to the console and setting the
